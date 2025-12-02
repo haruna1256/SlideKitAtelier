9da20bd7c16f3816bb2bad1e1a3576d47737891e
haruna1256 <haru_kawagishi@icloud.com>
2025-12-02 16:36:57 +0900

    [update] 色合いの変更

---
+++ SlideKitAtelier/Slides/TitleSlide.swift
@@ -0,0 +1,18 @@
+//
+//  TitleSlide.swift
+//  SlideKitAtelier
+//
+//  Created by 川岸遥奈 on 2025/12/02.
+//
+
+import SwiftUI
+
+struct TitleSlide: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TitleSlide()
+}
