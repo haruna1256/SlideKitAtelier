22e18ab9c18f673c41b75ae3a7bb754dfbed361a
haruna1256 <haru_kawagishi@icloud.com>
2025-12-04 10:57:56 +0900

    [update] 配置の調整

--- SlideKitAtelier/Slides/TitleSlide.swift
+++ SlideKitAtelier/Slides/TitleSlide.swift
@@ -17,24 +17,29 @@ struct TitleSlide: View {
 
             VStack(spacing: 24) {
                 // ゼミのタイトル
-                HStack(spacing: 48) {
+                HStack(alignment: .center, spacing: 40) {
                     Image("swift")
                         .resizable()
+                        .scaledToFit()
                         .frame(width: 320, height: 320)
+                        .padding(.leading, 32)
 
-                    VStack {
                         Spacer()
-                        Text("SlideKit を用いたインタラクティブ資料の設計")
+                        Text("SlideKit を用いた\nインタラクティブ資料の設計")
                             .font(Font.custom("KiwiMaru-Medium", size: 72))
                             .foregroundColor(.white)
-                    }
-                    .frame(height: 220)
-                }
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                                  }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 40)
+                .padding(.horizontal, 40)
 
                 // ゼミタイトル・自分の名前
                 HStack(spacing: 32) {
                     Image("swift")
                         .resizable()
+                        .scaledToFit()
                         .frame(width: 380, height: 350)
                     VStack(spacing: 16) {
                         Text("WEB・モバイルゼミ　最終発表")
